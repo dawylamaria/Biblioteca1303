2d2cf76344dd9b8af2befabc5a9c4e24fcca827f
luisf005 <125405472+luisf005@users.noreply.github.com>
2023-03-14 08:23:48 -0300

    Update ObrasVisuais.java

--- src/br/edu/fescfafic/biblioteca/Model/ObrasVisuais.java
+++ src/br/edu/fescfafic/biblioteca/Model/ObrasVisuais.java
@@ -1,5 +1,32 @@
 package br.edu.fescfafic.biblioteca.Model;
 
 public class ObrasVisuais {
-    //- Quadros e esculturas (tipo, código identificador, autor, ano, disponivel#boolean?, exposto#boolean)
+
+    public String tipo;
+    public String codigoID;
+    public String nomeAutor;
+    public String ano;
+    public boolean disponivel;
+    public boolean exposto;
+
+
+    public ObrasVisuais(String tipo, String codigo, String autor, String ano, boolean disponivel, boolean exposto){
+
+        this.tipo = tipo;
+        this.codigoID = codigo;
+        this.nomeAutor = autor;
+        this.ano = ano;
+        this.disponivel = disponivel;
+        this.exposto = exposto;
+
+    }
+
+    public void setDisponivel(boolean status){
+        this.disponivel = status;
+    }
+    
+    public void setExposto(boolean status){
+        this.exposto = status;
+    }
+    
 }
