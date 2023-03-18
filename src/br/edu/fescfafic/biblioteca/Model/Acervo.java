5fe92ab8733f5c67d37edb578034017603a0a0c9
luisf005 <125405472+luisf005@users.noreply.github.com>
2023-03-18 10:32:43 -0300

    Update Acervo.java

--- src/br/edu/fescfafic/biblioteca/Model/Acervo.java
+++ src/br/edu/fescfafic/biblioteca/Model/Acervo.java
@@ -1,20 +1,24 @@
 package br.edu.fescfafic.biblioteca.Model;
+import java.util.ArrayList;
 
 abstract class Acervo {
-/* Acervo(tipo, código identificador, autor, ano, disponivel#boolean?)
-Listas
-- Digital (livros, revistas)
-- Permanente( #indisponíveis para saída da biblioteca, quadros, esculturas, livros raros)
-- Fisico(livros, revistas)
-*/
-    public String genero;
-    public String codigoID;
-    public String nomeAutor;
+
+    public String tipo;
+    public String codigoIdentificador;
+    public String autor;
     public String ano;
     public boolean disponivel;
-
+    
     public ArrayList<String> listaAcervoDigital = new ArrayList<>();
     public ArrayList<String> listaAcervoFisico = new ArrayList<>();
     public ArrayList<String> listaAcervoPermanente = new ArrayList<>();
-    
+
+    Acervo(String tipo, String codigoIdentificador, String autor, String ano, boolean disponivel){
+        this.tipo = tipo;
+        this.codigoIdentificador = codigoIdentificador;
+        this.autor = autor;
+        this.ano = ano;
+        this.disponivel = disponivel;
+    }
+
 }
