bfdd09b3c7cdd79446bb5abf9c7df43d67c7e149
luisf005 <125405472+luisf005@users.noreply.github.com>
2023-03-18 12:23:59 -0300

    Update Leitor.java

--- src/br/edu/fescfafic/biblioteca/Model/Leitor.java
+++ src/br/edu/fescfafic/biblioteca/Model/Leitor.java
@@ -1,10 +1,12 @@
 package br.edu.fescfafic.biblioteca.Model;
 import java.util.ArrayList;
 
-public class Leitor extends Pessoa{
+public class Leitor extends Pessoa {
     public int idade;
     public double debito;
     public boolean cadastroLiberado;
+
+    public ArrayList<Leitor> listaDeLeitores = new ArrayList<>();
 
     public Leitor(String nome, int idade, String _cpf, String _rg, boolean cadastroLiberado, double debito){
         super(nome, _cpf, _rg);
@@ -14,8 +16,16 @@ public class Leitor extends Pessoa{
 
     }
 
+    public void cadastrarLeitores(Leitor leitores){
+        this.listaDeLeitores.add(leitores);
+    }
+
     public void setCadastroLiberado(boolean status) {
         this.cadastroLiberado = status;
+    }
+
+    public void getCadastroLiberado(){
+        System.out.println(this.cadastroLiberado);
     }
 
 }
