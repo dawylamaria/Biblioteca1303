e1dae464ac9aedbefcc507a58668ec3348920a59
luisf005 <125405472+luisf005@users.noreply.github.com>
2023-03-18 12:30:13 -0300

    Update Impressos.java

--- src/br/edu/fescfafic/biblioteca/Model/Impressos.java
+++ src/br/edu/fescfafic/biblioteca/Model/Impressos.java
@@ -1,24 +1,21 @@
 package br.edu.fescfafic.biblioteca.Model;
 
-public class Impressos extends Acervos {
+public class Impressos extends Acervo {
 
     public String edicao;
     public String genero;
     public String nomeDaObra;
-    public String nomeDoAutor;
     public String quantidadeDePaginas;
 
 
-    Impressos(String tipo, String codigoIdentificador, String autor, String ano, boolean disponivel, String edicao,
-              String genero, String nomeDaObra, String nomeDoAutor, String quantidadeDePaginas) {
+    public Impressos(String tipo, String codigoIdentificador, String autor, String ano, boolean disponivel, String edicao,
+              String genero, String nomeDaObra, String quantidadeDePaginas) {
         super(tipo, codigoIdentificador, autor, ano, disponivel);
         this.edicao = edicao;
         this.genero = genero;
         this.nomeDaObra = nomeDaObra;
-        this.nomeDoAutor = nomeDoAutor;
         this.quantidadeDePaginas = quantidadeDePaginas;
 
     }
 
 }
-
