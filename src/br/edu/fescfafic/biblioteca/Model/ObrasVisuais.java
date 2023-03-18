74bed313c1ece4229e510ae61a7de01d3415e611
luisf005 <125405472+luisf005@users.noreply.github.com>
2023-03-18 10:40:04 -0300

    Update ObrasVisuais.java

--- src/br/edu/fescfafic/biblioteca/Model/ObrasVisuais.java
+++ src/br/edu/fescfafic/biblioteca/Model/ObrasVisuais.java
@@ -1,32 +1,20 @@
 package br.edu.fescfafic.biblioteca.Model;
 
-public class ObrasVisuais {
-
-    public String tipo;
-    public String codigoID;
-    public String nomeAutor;
-    public String ano;
-    public boolean disponivel;
+public class ObrasVisuais extends Acervo {
     public boolean exposto;
 
 
-    public ObrasVisuais(String tipo, String codigo, String autor, String ano, boolean disponivel, boolean exposto){
-
-        this.tipo = tipo;
-        this.codigoID = codigo;
-        this.nomeAutor = autor;
-        this.ano = ano;
-        this.disponivel = disponivel;
+    public ObrasVisuais(String tipo, String codigoIdentificador, String autor, String ano, boolean disponivel, boolean exposto){
+        super(tipo, codigoIdentificador, autor, ano, disponivel);
         this.exposto = exposto;
-
     }
 
     public void setDisponivel(boolean status){
         this.disponivel = status;
     }
-    
+
     public void setExposto(boolean status){
         this.exposto = status;
     }
-    
+
 }
